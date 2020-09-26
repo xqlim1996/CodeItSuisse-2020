31987ae38a3ea86318aaf8f9a77f463963c5a69d
Xiao Qi Lim <xqlim1996@gmail.com>
2020-09-26 14:49:24 +0800

    contact-tracing tidying

--- codeitsuisse/routes/contact_tracing.py
+++ codeitsuisse/routes/contact_tracing.py
@@ -13,11 +13,6 @@ def evaluate():
     logging.info("data sent for evaluation {}".format(data))
     
     result = contact_trace(data)
-    # for test_case in data:
-    #     result.append(contact_trace(test_case))
-    
-
-    # logging.info("My result :{}".format(infected))
     return json.dumps(result)
 
 
@@ -38,51 +33,20 @@ def contact_trace(test_case): #input is dictionary
     graph_map = {}
     graph_map[origin_name] = []
 
-
-    #map among clusters and origin
-    # for name, genome in clusters.items():
-    #     if name == origin_name:
-    #         continue
-    #     min_nodes = []
-    #     for name_other, genome_other in clusters.items():
-    #         min_diff = 99999999
-    #         dist_dict = {} #format is name: [dist, non-silent]
-    #         if name != name_other:
-    #             diff_genome, non_silent = compare_genome(genome, genome_other)
-    #             # print('clusters')
-    #             # print(diff_genome, non_silent)
-    #             dist_dict[name_other] = [diff_genome, non_silent]
-    #             if diff_genome <= min_diff and diff_genome != 0:
-    #                 min_diff = diff_genome
-    #                 # min_nodes.append([name_other, non_silent])
-
-    #     # print(dist_dict)
-    #     for dist_name, details in dist_dict.items():
-    #         if details[0] == min_diff:
-    #             min_nodes.append([dist_name, details[1]])
-    #     graph_map[name] = min_nodes
-
     for name, genome in clusters.items():
         if name == origin_name:
             continue
         min_nodes = []
-        # print(name)
         min_diff = 99999999
         dist_dict = {} #format is name: [dist, non-silent]
         for name_other, genome_other in clusters.items():
             
             if name != name_other:
                 diff_genome, non_silent = compare_genome(genome, genome_other)
-                # print('clusters')
-                # print(diff_genome, non_silent)
-                print(name, name_other,diff_genome)
                 dist_dict[name_other] = [diff_genome, non_silent]
                 if diff_genome <= min_diff and diff_genome != 0:
                     min_diff = diff_genome
-                    # min_nodes.append([name_other, non_silent])
-                # print(min_diff)
-        # print(name)
-        # print(dist_dict)
+
         for dist_name, details in dist_dict.items():
             if details[0] == min_diff:
                 min_nodes.append([dist_name, details[1]])
@@ -94,11 +58,8 @@ def contact_trace(test_case): #input is dictionary
     dist_dict = {} #format is name: [dist, non-silent]
     min_diff = 99999999
     for name, genome in clusters.items():
-        # print('name: ' + name)
         diff_genome, non_silent = compare_genome(genome, infected_genome)
         dist_dict[name] = [diff_genome, non_silent]
-        # print('infected')
-        # print(diff_genome, non_silent)
         if diff_genome <= min_diff:
             min_diff = diff_genome
 
@@ -106,12 +67,8 @@ def contact_trace(test_case): #input is dictionary
         if details[0] == min_diff:
             infected_map.append([name, details[1]])
 
-            # infected_map.append([name, non_silent])
 
     graph_map[infected_name] = infected_map
-    print('GRAPH:')
-    print(graph_map)
-
     paths = []
     path_counter = len(clusters)
     #worst case is go through every cluster and origin
@@ -121,12 +78,9 @@ def contact_trace(test_case): #input is dictionary
         
     temp = paths.copy()
     output = []
-    # print('PATH')
-    # print('infected paths:' + str(paths))
+
     while len(temp) > 0:
-        # print('temp: {}'.format(temp))
         for index in range(len(paths)):
-            # print('LOOP')
             path = paths[index]
             last_node = path[-1]
             
@@ -136,27 +90,25 @@ def contact_trace(test_case): #input is dictionary
                 next_nodes = graph_map[last_node[0]]
 
             if len(next_nodes) == 0: #no more next node
-                # print('temp removing path: ' + str(path))
                 output.append(path)
                 temp.remove(path)
             elif len(next_nodes) == 1:
                 
                 path.append(next_nodes[0])
                 temp[index] = path
-                # print('temp updating path: ' + str(path))
+
             else:
                 path.append(next_nodes[0])
                 for node in next_nodes[1:]:
                     new_path = path.copy()
                     new_path.append(node)
                     temp.append(new_path)
-                    # print('temp adding new path' + str(new_path))
+
         paths = temp.copy()
-        # print('paths:' + str(paths))
+
 
     #format output
     
-    # print(output)
     output_format = []
 
     for path in output:
@@ -171,9 +123,6 @@ def contact_trace(test_case): #input is dictionary
                 path_format += node[0]
         output_format.append(path_format)
 
-    print('OUTPUT')
-    print(output_format)
-
     return output_format
                 
 def compare_genome(genome1, genome2):
@@ -183,29 +132,20 @@ def compare_genome(genome1, genome2):
 
     for index in range(len(genome1)):
         if genome1[index] != genome2[index]: #check for num diff char and if first char diff
-            print('AFAS')
 
             genome1_instr = list(genome1[index])
             genome2_instr = list(genome2[index])
 
             #compare num diff in instr.
             diff_instr = compare_instr(genome1_instr, genome2_instr)
-            print(diff_instr)
             diff_genome += diff_instr[0]
             first_char_diff_genome += diff_instr[1]
-            # print('silent')
     if first_char_diff_genome > 1:
         non_silent = True
     
     return diff_genome, non_silent
 
-        # if infected_genome[index] != origin_genome[index]:
-        #     infect_instr = infected_genome[index].split()
-        #     origin_instr = origin_genome[index].split()
 
-        #     diff_origin = compare_instr(infect_instr, cluster_instr)
-        #     diff_arr[1] += diff_origin[0]
-        #     first_char_diff_arr[1] += diff_origin[1]
                 
 def compare_instr(instr1, instr2):
     print('instr1: ' + str(instr1))
@@ -213,10 +153,9 @@ def compare_instr(instr1, instr2):
 
     diff = 0
     first_char_diff = 0
-    # print(instr_1, instr_2)
+
     for char_index in range(len(instr1)):
-        # print(instr1[char_index])
-        # print(instr1[char_index])
+        
         if instr1[char_index] != instr2[char_index]:
             if char_index == 0:
                 first_char_diff += 1
@@ -225,42 +164,3 @@ def compare_instr(instr1, instr2):
     return [diff, first_char_diff]
 
     
-def main():
-#     test = {
-#     "infected": {
-#         "name":"orange",
-#         "genome":"acg-gcu-uca-gca-acu-ccc-gua-acg-gcu-uca-gca-acu-cac-gaa"
-#     },
-#     "origin": {
-#         "name":"turquoise",
-#         "genome":"acg-gcu-uca-gca-acu-ccc-gua-acg-gcu-uca-gca-acu-cac-gaa"
-#     },
-#     "cluster":[
-#         {
-#             "name":"blue",
-#             "genome":"acg-gcu-uca-gca-acu-ccc-gua-acg-gcu-uca-gca-acu-cac-gaa"
-#         },
-#         {
-#             "name":"magenta",
-#             "genome":"aca-gcu-uca-gca-acu-ccc-gua-acg-gcu-uca-gca-acu-cac-gaa"
-#         }
-#     ]
-# }
-
-    test = {
-    "infected": {
-        "name":"orange",
-        "genome":"acg-gcu-uca-gca-acu-ccc-gua-acg-gcu-uca-gca-acu-cac-gaa"
-    },
-    "origin": {
-        "name":"turquoise",
-        "genome":"acg-gcu-uca-gca-acu-ccc-gua-acg-gcu-uca-gca-acu-cac-gaa"
-    },
-    "cluster":[
-        {
-            "name":"magenta",
-            "genome":"acg-gcu-uca-gca-acu-ccc-gua-acg-gcu-uca-gca-acu-cac-gaa"
-        }
-    ]
-}
-    return(contact_trace(test))
