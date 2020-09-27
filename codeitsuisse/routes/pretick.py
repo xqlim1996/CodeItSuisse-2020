e72e700cc27789a5c117ff878ff7bd84f93534ee
Xiao Qi Lim <xqlim1996@gmail.com>
2020-09-27 08:44:46 +0800

    pretick fix

--- codeitsuisse/routes/pretick.py
+++ codeitsuisse/routes/pretick.py
@@ -9,9 +9,10 @@ logger = logging.getLogger(__name__)
 
 @app.route('/pre-tick', methods=['POST'])
 def evaluate_pretick():
-    data = request.get_json();
+    data = request.data
+    # data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input");
+    # inputValue = data.get("input");
     result = 100
     # logging.info("My result :{}".format(result))
 
