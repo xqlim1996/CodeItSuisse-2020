b7eaa95889d54be0b6a1bfec27fe95d199994b3f
Xiao Qi Lim <xqlim1996@gmail.com>
2020-09-27 08:39:29 +0800

    pretick test

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -3,6 +3,7 @@ app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.contact_tracing
 import codeitsuisse.routes.inventory_management
+import codeitsuisse.routes.pretick
 
 
 
---
+++ codeitsuisse/routes/pretick.py
@@ -0,0 +1,21 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+@app.route('/pre-tick', methods=['POST'])
+def evaluate_pretick():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    inputValue = data.get("input");
+    result = 100
+    # logging.info("My result :{}".format(result))
+
+    return json.dumps(result);
+
+
+
